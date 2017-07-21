6ed5eb816a1d6bb0bda2cac4bdd378f8f7fb137b
jiashunming <jiashunming@outlook.com>
2017-07-21 14:25:11 +0800

    update

--- Node/mixture.js
+++ Node/mixture.js
@@ -1,5 +1,11 @@
 /**
  * 将一些比较标准的回掉API转换为Promise
+ * 支持如下三种使用方式：
+ * const fs = require('fs');
+ *
+ * const writeFile = mixture(fs.writeFile) // 1
+ * const [mkdir, unlink] = mixture([fs.mkdir, fs.unlink]) // 2
+ * const [readFile, stat] = mixture(fs.readFile, fs.stat) // 3
  */
 
 module.exports = mixture;
